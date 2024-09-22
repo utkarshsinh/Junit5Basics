89797659e92cbe50af6656c636b0ed4c0ef099dc
utkarshsinh <usinh002@ucr.edu>
2024-09-22 00:46:42 -0700

    Add lifecycle management to test class
    Introduced @TestInstance(PER_METHOD) to manage test instance lifecycle. Also, added a StringBuilder field to track the order of test method executions.

--- src/test/java/org/example/MethodOrderedByOrderedIndex.java
+++ src/test/java/org/example/MethodOrderedByOrderedIndex.java
@@ -1,33 +1,35 @@
 package org.example;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MethodOrderedByOrderedIndex {
-
+    StringBuilder completed = new StringBuilder("");
     @Order(4)
     @Test
     void testC(){
         System.out.println("Running test C");
+        completed.append("4");
     }
     @Order(3)
     @Test
     void testD(){
         System.out.println("Running test D");
+        completed.append("3");
     }
     @Order(1)
     @Test
     void testA(){
         System.out.println("Running test A");
+        completed.append("1");
     }
 
     @Order(2)
     @Test
     void testB(){
         System.out.println("Running test B");
+        completed.append("2");
     }
 
 
